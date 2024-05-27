f9a8500a8540009253371c47b202e5299bb9d955
Induwara Uthsara <induwarauthsara19@gmail.com>
2024-05-27 19:25:43 +0530

    style and typo erros

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,6 @@
 
 export default function Container({ children }) {
     return (
-        <div className="max-w-[500px] mx-auto flex flex-col min-h-screen bg-white border-l border-r drop-shadow-xl">{children}</div>
+        <div className="max-w-[1100px] mx-auto flex flex-col min-h-screen bg-white border-l border-r drop-shadow-xl">{children}</div>
     )
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
         <footer className="items-center py-5 px-7 border-t mt-auto text-center">
-            <p className="text-sm">© 2030 Induwara Uthsara. All rights reserved.</p>
+            <p className="text-sm">© 2023 Induwara Uthsara. All rights reserved.</p>
         </footer>
     )
 }
