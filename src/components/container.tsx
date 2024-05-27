6658006e2395dc77193b635243b039ff1b618423
Induwara Uthsara <induwarauthsara19@gmail.com>
2024-05-27 19:32:37 +0530

    Type error: Binding element 'children' implicitly has an 'any' type.

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,5 +1,6 @@
+import React, { ReactNode } from 'react';
 
-export default function Container({ children }) {
+export default function Container({ children }: { children: React.ReactNode }) {
     return (
         <div className="max-w-[1100px] mx-auto flex flex-col min-h-screen bg-white border-l border-r drop-shadow-xl">{children}</div>
     )
